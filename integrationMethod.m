e3157988779fd79285a41fd4f376d60708b70c97
Cristian Garcia <cristiaangz@gmail.com>
2019-12-08 20:44:21 +0400

    fix log

--- integrationMethod.m
+++ integrationMethod.m
@@ -37,6 +37,7 @@ for i = (1:m)
     xdash1(i,3) = (S1(i+1)*S2(i+1) + S1(i)*S2(i))/2;
 end
 
+global a1
 a1 = inv(transpose(xdash1)*xdash1)*transpose(xdash1)*d1;
 
 r1 = a1(1)
@@ -55,6 +56,7 @@ for i = (1:m)
     xdash2(i,3) = (S1(i+1)*S2(i+1) + S1(i)*S2(i))/2;
 end
 
+global a2
 a2 = inv(transpose(xdash2)*xdash2)*transpose(xdash2)*d2;
 
 r2 = a2(1)
@@ -62,17 +64,20 @@ k2 = -r2/a2(2)
 alpha21 = -(a2(3)*k2)/r2
 
 % %Resimulate the results [NOT Working]
-% tspan = [0 10];
-% x_init = [0.01 0.02];
-% 
-% [TEMP1, TEMP2] = odefun(dx1,dx2,a1,a2)
-% [dx1, dx2] = ode45(@(dx1, dx2) odefun(dx1,dx2,a1,a2), tspan, x_init);
-% 
-%  
-% 
-% function [dx1, dx2] = odefun(x1, x2, a1, a2)    
-%     dx1 = x1*(a1(1) + a1(2)*x1 +a1(3)*x2);
-%     dx2 = x2*(a2(1) + a2(2)*x2 +a2(3)*x1);
-% end
+tspan = [0 10];
+x_init = [0.01 0.02];
+
+[dx1, dx2] = ode45(odefun, tspan, x_init);
+
+ 
+
+function [dx1, dx2] = odefun(x1, x2)
+    global a1
+    a1
+    global a2
+    a2
+    dx1 = x1*(a1(1) + a1(2)*(x1^2) +a1(3)*x2*x2);
+    dx2 = x2*(a2(1) + a2(2)*x2 +a2(3)*x1);
+end
 
 
--- logIntegralMethod.m
+++ logIntegralMethod.m
@@ -16,28 +16,36 @@ n = 2;
 
 % Solving for Species 1
 
-d1 = zeros(m, 1);
+l1 = zeros(m, 1);
 xdash1 = ones(m, n);
 
 for i = (1:m)
-    d1(i) = (log(S1(i+1)) - log(S1(i)));
+    l1(i) = (log(S1(i+1)) - log(S1(i)));
     xdash1(i,2) = (S1(i+1) + S1(i))/2;
     xdash1(i,3) = (S2(i+1) + S2(i))/2;
 end
 
 
-a1 = inv(transpose(xdash1)*xdash1)*transpose(xdash1)*eye(m,1)
+a1 = inv(transpose(xdash1)*xdash1)*transpose(xdash1)*l1;
+
+r1 = a1(1)
+k1 = -r1/a1(2)
+alpha12 = -(a1(3)*k1)/r1
 
 % Solving for Species 2
 
-d2 = zeros(m, 1);
+l2 = zeros(m, 1);
 xdash2 = ones(m, n);
 
 for i = (1:m)
-    d2(i) = (log(S2(i+1)) - log(S2(i)));
+    l2(i) = (log(S2(i+1)) - log(S2(i)));
     xdash2(i,2) = (S2(i+1) + S2(i))/2;
     xdash2(i,3) = (S1(i+1) + S1(i))/2;
 end
 
 
-a2 = inv(transpose(xdash2)*xdash2)*transpose(xdash2)*eye(m,1)
+a2 = inv(transpose(xdash2)*xdash2)*transpose(xdash2)*l2;
+
+r2 = a2(1)
+k2 = -r2/a2(2)
+alpha21 = -(a2(3)*k2)/r2
