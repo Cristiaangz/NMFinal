918e67b4f25a31ebff32e915f91c7eb4162aae37
Cristian Garcia <cristiaangz@gmail.com>
2019-12-09 10:42:23 +0400

    Fixed integration method to work at the proper scale. The issue was that we forgot to divide our A coefficients by the sampling rate. it now works properly in both ODEs

--- integrationMethod.m
+++ integrationMethod.m
@@ -15,15 +15,12 @@ clear
 load X1
 load X2
 
-% hold on
-% title("Analyzed Timeseries")
-% scatter(X1)
-% scatter(X2)
-
 S1 = X1.data;
 S2 = X2.data;
 m = length(S1)-1;
 n = 2;
+global sampleRate
+sampleRate = 0.1;
 
 % Solving for Species 1
 
@@ -38,10 +35,10 @@ for i = (1:m)
 end
 
 global a1
-a1 = inv(transpose(xdash1)*xdash1)*transpose(xdash1)*d1;
+a1 = (inv(transpose(xdash1)*xdash1)*transpose(xdash1)*d1)/sampleRate;
 
 global r1 k1 alpha12;
-r1 = 10*a1(1);
+r1 = a1(1);
 k1 = -r1/a1(2);
 alpha12 = -(a1(3)*k1)/r1;
 
@@ -58,10 +55,10 @@ for i = (1:m)
 end
 
 global a2
-a2 = inv(transpose(xdash2)*xdash2)*transpose(xdash2)*d2;
+a2 = (inv(transpose(xdash2)*xdash2)*transpose(xdash2)*d2)/sampleRate;
 
 global r2 k2 alpha21;
-r2 = 10*a2(1);
+r2 = a2(1);
 k2 = -r2/a2(2);
 alpha21 = -(a2(3)*k2)/r2;
 
@@ -70,10 +67,10 @@ alpha21 = -(a2(3)*k2)/r2;
 tspan = [0 10];
 x_init = [0.01 0.02];
 [t,y] = ode45(@mysysfun,tspan,x_init);
-% dy1 = gradient(y(:,1));
-% dy2 = gradient(y(:,2));
 x2_fit = y(:,2);
 x1_fit = y(:,1);
+
+
 %% Verification 1 on a matrix: Checking Overall Shape 1)Original vs. 2)Fitted
 figure(1)
 sgtitle("Ver.1--Checking Overall Shape 1)Original vs. 2)Fitted")
@@ -119,15 +116,15 @@ hold off
 tspan = [0 10];
 x_init = [0.01 0.02];
 [t,y] = ode45(@mysysfun2,tspan,x_init);
-% % dy1 = gradient(y(:,1));
-% % dy2 = gradient(y(:,2));
+
+
 x2_fit_ = y(:,2);
 x1_fit_ = y(:,1);
 x2_reg = x2_fit_;
 x1_reg = x1_fit_;
 
 %% Verification 4: Checking Overall Shape 1)Original vs. 2)Fitted
-% Scale is off by 10?
+% Scale works now
 figure(4)
 sgtitle("Ver.4--Checking Overall Shape 1)Original vs. 2)Fitted useful parameter")
 subplot(2,1,1);
@@ -183,16 +180,16 @@ hold off
 %% Functions
 function f = mysysfun(t,X)
 
-multiplier = 10;
 global a1
 global a2
-f(1,1) = X(1)*a1(1)*multiplier + (X(1)^2)*a1(2) + X(1)*X(2)*a1(3);
-f(2,1) = X(2)*a2(1)*multiplier + (X(2)^2)*a2(2) + X(2)*X(1)*a2(3);
+global sampleRate
+f(1,1) = (X(1)*a1(1) + (X(1)^2)*a1(2) + X(1)*X(2)*a1(3));
+f(2,1) = (X(2)*a2(1) + (X(2)^2)*a2(2) + X(2)*X(1)*a2(3));
 end
 
 function f = mysysfun2(t,X)
 
 global r1 k1 alpha12 r2 k2 alpha21;
-f(1,1) = r1*X(1) - (r1*X(1)^2)/k1 - (r1*alpha12*X(2)*X(1))/k1;
-f(2,1) = r2*X(2) - (r2*X(2)^2)/k2 - (r2*alpha21*X(2)*X(1))/k2;
+f(1,1) = r1*X(1) - ((r1*X(1)^2)/k1) - ((r1*alpha12*X(2)*X(1))/k1);
+f(2,1) = r2*X(2) - ((r2*X(2)^2)/k2) - ((r2*alpha21*X(2)*X(1))/k2);
 end
