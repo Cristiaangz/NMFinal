ae5f9b2471e5583fa76409b45fbea10d25392ee7
Cristian Garcia <cristiaangz@gmail.com>
2019-12-11 13:52:06 +0400

    Changed code to handle both clean and noisy datasets at one. Calculates regression's variance compared to clean parameters

--- Computational Methods/integrationMethod.m
+++ Computational Methods/integrationMethod.m
@@ -12,18 +12,24 @@
 
 %% Initialisation 
 
-load X1
-load X2
+clear
+clc
 
-dataTimeSeries = X1.time;
+load X1_Noisy
+load X1_Clean
+load X2_Noisy
+load X2_Clean
 
-speciesOne = X1.data;
-speciesTwo = X2.data;
+TimeSeries = (speciesOne_Clean(1,:)).';
+speciesOne_Clean = (speciesOne_Clean(2, :)).';
+speciesOne_Noisy = (speciesOne_Noisy(2, :)).';
+speciesTwo_Clean = (speciesTwo_Clean(2, :)).';
+speciesTwo_Noisy = (speciesTwo_Noisy(2, :)).';
 
-m = length(speciesOne)-1;
+m = length(speciesOne_Noisy)-1;
 n = 2;
 
-sampleRate = dataTimeSeries(2)-dataTimeSeries(1);
+sampleRate = TimeSeries(2)-TimeSeries(1);
 
 %% Solving for Species 1
 
@@ -31,10 +37,10 @@ dMatrixOne = zeros(m, 1);
 xBarOne = zeros(m, n);
 
 for i = (1:m)
-    dMatrixOne(i) = (speciesOne(i+1) - speciesOne(i))/sampleRate;
-    xBarOne(i,1) = (speciesOne(i+1) + speciesOne(i))/2;
-    xBarOne(i,2) = ((speciesOne(i+1))^2 + (speciesOne(i))^2)/2;
-    xBarOne(i,3) = (speciesOne(i+1)*speciesTwo(i+1) + speciesOne(i)*speciesTwo(i))/2;
+    dMatrixOne(i) = (speciesOne_Noisy(i+1) - speciesOne_Noisy(i))/sampleRate;
+    xBarOne(i,1) = (speciesOne_Noisy(i+1) + speciesOne_Noisy(i))/2;
+    xBarOne(i,2) = ((speciesOne_Noisy(i+1))^2 + (speciesOne_Noisy(i))^2)/2;
+    xBarOne(i,3) = (speciesOne_Noisy(i+1)*speciesTwo_Noisy(i+1) + speciesOne_Noisy(i)*speciesTwo_Noisy(i))/2;
 end
 
 global a1 r1 k1 alpha12;
@@ -49,10 +55,10 @@ d2 = zeros(m, 1);
 xdash2 = zeros(m, n);
 
 for i = (1:m)
-    d2(i) = (speciesTwo(i+1) - speciesTwo(i))/sampleRate;
-    xdash2(i,1) = (speciesTwo(i+1) + speciesTwo(i))/2;
-    xdash2(i,2) = ((speciesTwo(i+1))^2 + (speciesTwo(i))^2)/2;
-    xdash2(i,3) = (speciesOne(i+1)*speciesTwo(i+1) + speciesOne(i)*speciesTwo(i))/2;
+    d2(i) = (speciesTwo_Noisy(i+1) - speciesTwo_Noisy(i))/sampleRate;
+    xdash2(i,1) = (speciesTwo_Noisy(i+1) + speciesTwo_Noisy(i))/2;
+    xdash2(i,2) = ((speciesTwo_Noisy(i+1))^2 + (speciesTwo_Noisy(i))^2)/2;
+    xdash2(i,3) = (speciesOne_Noisy(i+1)*speciesTwo_Noisy(i+1) + speciesOne_Noisy(i)*speciesTwo_Noisy(i))/2;
 end
 
 global a2 r2 k2 alpha21;
@@ -63,92 +69,97 @@ alpha21 = -(a2(3)*k2)/r2;
 
 %% Resimulate  Results by Solving System of ODE's using A's
 
-tspan = linspace(0, dataTimeSeries(end), (dataTimeSeries(end)/sampleRate)+1);
+tspan = linspace(0, TimeSeries(end), (TimeSeries(end)/sampleRate)+1);
 x_init = [0.01 0.02];
 [t,y] = ode45(@mysysfun,tspan,x_init);
-x2_fit = y(:,2);
-x2_error = speciesTwo - x2_fit;
-x1_fit = y(:,1);
-x1_error = speciesOne - x1_fit;
+x2_raw_fit = y(:,2);
+x1_raw_fit = y(:,1);
 
 
-%% Verification 1: Checking Overall Shape 1)Original vs. 2)Fitted
+
+%% Verification 1: Noisy Signal and Raw Regression
 
 figure(1)
 
-title("Ver.1--Checking Overall Shape 1)Original vs. 2)Fitted")
+title("Noisy Signal and Raw Regression")
 
 hold on;
 
-scatter(dataTimeSeries, speciesOne)
-scatter(dataTimeSeries, speciesTwo)
+scatter(TimeSeries, speciesOne_Noisy)
+scatter(TimeSeries, speciesTwo_Noisy)
 
-plot(t,x1_fit);
-plot(t,x2_fit);
+plot(t,x1_raw_fit);
+plot(t,x2_raw_fit);
 
 legend("S1","S2", "S1 Calculated", "S2 Calculated")
 
 hold off
-%% Verification 2: 1)X1 2)X2
+
+%% Error Calculations for Raw Regresion
+
+x1_error = speciesOne_Clean - x1_raw_fit;
+x2_error = speciesTwo_Clean - x2_raw_fit;
+var_raw_x1 = sum(x1_error.^2)/(length(TimeSeries)-2);
+var_raw_x2 = sum(x2_error.^2)/(length(TimeSeries)-2);
 
 figure(2)
 
-title("Ver.2-- 1)X1 2)X2")
+subplot(2,1,1)
+title("Calculated vs. Expected Species One")
+scatter(x1_raw_fit, speciesOne_Clean);
 
-subplot(2,1,1);
+subplot(2,1,2)
+title("Calculated vs. Expected Species Two")
+scatter(x2_raw_fit, speciesTwo_Clean);
 
-hold on;
 
-plot(X1)
-plot(t,x1_fit)
 
-legend("X1","Fitted X1")
-
-hold off;
-
-subplot(2,1,2);
-
-hold on;
-
-plot(X2)
-plot(t,x2_fit)
-
-legend("X2","Fitted X2")
-
-hold off
 %% Resimulate the results by Solving System of ODE's using useful parameters
 
-tspan = linspace(0, dataTimeSeries(end), (dataTimeSeries(end)/sampleRate)+1);
+tspan = linspace(0, TimeSeries(end), (TimeSeries(end)/sampleRate)+1);
 x_init = [0.01 0.02];
 [t,y] = ode45(@mysysfun2,tspan,x_init);
 
 
-x2_reg = y(:,2);
-x1_reg = y(:,1);
+x2_reg_fit = y(:,2);
+x1_reg_fit = y(:,1);
 
-x1_reg_error = speciesOne - x1_reg;
-x2_reg_error = speciesTwo - x2_reg;
 
-variance_x1 = sum(x1_reg_error.^2)/(length(dataTimeSeries)-2)
-variance_x2 = sum(x2_reg_error.^2)/(length(dataTimeSeries)-2)
-
-%% Verifcation 3: Verifying matrix fits with useful parameters
+%% Verification 2: Noisy Signal and Regularized Regression
 
 figure(3)
 
+title("Noisy Signal and Raw Regression")
+
 hold on;
 
-plot(X1)
-plot(t,x1_fit)
-plot(X2)
-plot(t,x2_fit)
-plot(t,x1_reg)
-plot(t,x2_reg)
+scatter(TimeSeries, speciesOne_Noisy)
+scatter(TimeSeries, speciesTwo_Noisy)
 
-title("Ver.3 -- Checking all shapes, both method give same graph")
-legend("X1","Fitted X1","X2","Fitted X2","Param Fit X1","Param Fit X2")
+plot(t,x1_reg_fit);
+plot(t,x2_reg_fit);
+
+legend("S1","S2", "S1 Calculated", "S2 Calculated")
 
 hold off
+
+%% Error Calculations for Regularized Regresion
+x1_reg_error = speciesOne_Clean - x1_reg_fit;
+x2_reg_error = speciesTwo_Clean - x2_reg_fit;
+
+var_reg_x1 = sum(x1_reg_error.^2)/(length(TimeSeries)-2);
+var_reg_x2 = sum(x2_reg_error.^2)/(length(TimeSeries)-2);
+
+figure(4)
+
+subplot(2,1,1)
+title("Calculated vs. Expected Species One")
+scatter(x1_reg_fit, speciesOne_Clean);
+
+subplot(2,1,2)
+title("Calculated vs. Expected Species Two")
+scatter(x2_reg_fit, speciesTwo_Clean);
+
 %% Functions
 function f = mysysfun(t,X)
 
